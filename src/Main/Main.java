1aa8167080fec7e3dea0051406ce82dc089ec104
Blacksuan19 <Abubakaryagob@gmail.com>
2019-03-29 02:10:12 +0800

    Initial upload
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

---
+++ src/Main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../Splash/Splash.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 349));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/Splash/SplashController.java
@@ -0,0 +1,50 @@
+package Splash;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class SplashController implements Initializable {
+    @FXML
+    private AnchorPane rootPane;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+            new Splash().start();
+    }
+    class Splash extends Thread{
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000);
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        Parent root;
+                        try {
+                            root = FXMLLoader.load(getClass().getResource("../Splash/Splash.fxml"));
+                            Scene scene = new Scene(root);
+                            Stage stage = new Stage();
+                            stage.setScene(scene);
+                            stage.show();
+                            rootPane.getScene().getWindow().hide();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
