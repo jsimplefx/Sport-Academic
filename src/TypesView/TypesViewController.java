ae8814cd0e339042966edca6540ced995441b900
Blacksuan19 <Abubakaryagob@gmail.com>
2019-03-30 02:04:28 +0800

    Type View: link Squash
    * open questions view when one of the border panes is selected
    * pass view name to be used later (for setting the questions on the view)
    
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- src/TypesView/TypesViewController.java
+++ src/TypesView/TypesViewController.java
@@ -1,4 +1,68 @@
 package TypesView;
 
-public class TypesViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TypesViewController implements Initializable {
+
+    @FXML
+    private GridPane rootPane;
+
+    @FXML
+    private BorderPane typeA;
+
+    @FXML
+    private BorderPane typeB;
+
+    @FXML
+    private BorderPane typeC;
+
+    @FXML
+    private BorderPane typeD;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    void selType(MouseEvent event) {
+        if (event.getSource().equals(typeA)){
+            startQuiz("typeA");
+        }
+        if (event.getSource().equals(typeB)){
+            startQuiz("typeB");
+        }
+        if (event.getSource().equals(typeC)){
+            startQuiz("typeC");
+        }
+        if (event.getSource().equals(typeD)){
+            startQuiz("typeD");
+        }
+    }
+
+    private void startQuiz(String Sauce) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../QuesView/QuesView.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+            rootPane.getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
