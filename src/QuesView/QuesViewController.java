4dd8843c484df299167584df473dc1dfc7583e22
Blacksuan19 <Abubakaryagob@gmail.com>
2019-03-30 02:03:42 +0800

    Initial Quesitons View
    all the magic is finna happen here
    
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

---
+++ src/QuesView/QuesViewController.java
@@ -0,0 +1,14 @@
+package QuesView;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QuesViewController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
